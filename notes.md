ab53d82b59400b4ae7803916a7dec8df8add77f3
evanjohns <evanlloydjohns@gmail.com>
2022-02-06 12:15:59 -0500

    Added some notation

---
+++ notes.md
@@ -0,0 +1,19 @@
+
+What is a vector in Computer Science?
+
+    Vector containers are implemented as dynamic arrays; Just as regular arrays, vector containers have their elements 
+    stored in contiguous storage locations, which means that their elements can be accessed not only using iterators 
+    but also using offsets on regular pointers to elements.
+
+    But unlike regular arrays, storage in vectors is handled automatically, allowing 
+    it to be expanded and contracted as needed.
+    - Adam Davis
+*Source: https://stackoverflow.com/questions/508374/what-are-vectors-and-how-are-they-used-in-programming*
+
+C vector API:
+*https://en.cppreference.com/w/cpp/container/vector*
+
+memcpy
+
+    memcpy may be used to implicitly create objects in the destination buffer
+*https://en.cppreference.com/w/cpp/string/byte/memcpy*
--- student_lite_vector.c
+++ student_lite_vector.c
@@ -3,18 +3,93 @@
 #include <stdio.h>
 #include <string.h>
 
-lite_vector* lv_new_vec(size_t type_size){
+/*
+ * Vector is a dynamic "array", should be able to increase in size, but not decrease
+ * vector struct:
+ * typedef struct lv {
+ *	  size_t length;
+ *	  size_t max_capacity;
+ *	  size_t type_size;
+ *	  void** data;
+ *} lite_vector;
+ *
+ * Notes say that type_size in lite_vector is useless, don't use it
+ *
+ * ArrayList = Vector
+ * A bucket of elements
+ *      Ask for chunk of memory
+ *      Has specific amount of elements
+ *      Starts to fill up
+ *      Ask computer for more memory
+ *      Copy all elements in Vec1 to new malloc'd Vec2
+ *      Free Vec1
+ *      Storing pointers inside of arraylist
+ *          Data type is void*
+ *          lv_data = void** because it points to a void*
+ *          Malloc to some size you want to start out with (10, 100)
+ *          When you fill it up, ask for more.
+ *              How much more? idk pick something, 2x original size or 1.67x
+ *              resize does this
+ *              use memcpy to do the vec1 to vec2 copy
+ *
+ *
+ *   Two things you can do in the first function
+ *      Has to return a lite_vector pointer,
+ *          this implies that it has to go on the heap
+ *              Mechanism for getting heap memory is malloc
+ *              lite_vector HOLD data (void**)
+ *                  We have to malloc DATA void** too!!!!!1
+ *
+ */
+
+/**
+ * Create a new lite_vector.
+ *
+ * @param type_size The number of bytes for a single element of the type
+ *                  to be stored in the vector.
+ * @return The vector, or NULL if the operation fails.
+ */
+lite_vector* lv_new_vec(size_t type_size) {
+    lite_vector* tmp = malloc(sizeof(lite_vector)); // allocate nbits of memory to the pointer
+    tmp->data = malloc(sizeof(void**));
 }
 
+/**
+ * Free all memory used by the vector.  Should be called
+ * whenever the vector is no longer needed.
+ *
+ * @param vec The address of the vector we are finished using.
+ */
 void lv_cleanup(lite_vector* vec){
+    free(vec);
 }
 
+/**
+ * Get the current number of elements stored in the vector.
+ *
+ * @param vec The address of the vector we want to know the length of.
+ * @return The vector length, or 0 if the operation fails.
+ */
 size_t lv_get_length(lite_vector* vec){
 }
 
+/**
+ * Clear the contents of the vector and reset it to a default state.
+ *
+ * @param vec The address of the vector we wish to clear.
+ * @retun Sends true on success, or false otherwise.
+ */
 bool lv_clear(lite_vector* vec){
 }
 
+/**
+ * Get an element from an index from a vector.
+ *
+ * @param vec The address of the vector we wish to retrieve from.
+ * @param index The index we wish to retrieve from.
+ * @return An element from the vector.  NULL if doesn't exist or
+ *	   the function cannot complete.
+ */
 void* lv_get(lite_vector* vec, size_t index){
 }
 
@@ -31,5 +106,12 @@ void* lv_get(lite_vector* vec, size_t index){
 static bool lv_resize(lite_vector* vec){
 }
 
+/**
+ * Add an element to the vector, and extend the vector if needed.
+ *
+ * @param vec The address of the vector we wish to add to.
+ * @param element The element we wish to add.
+ * @return Will return true if successful, false otherwise.
+ */
 bool lv_append(lite_vector* vec, void* element){
 }
